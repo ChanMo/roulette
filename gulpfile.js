4e0dfc17ed368d3a06f8d7fa01c33931e6502c9a
ChanMo <chen.orange@aliyun.com>
2016-04-05 19:09:27 +0800

    fixed template error and add sass error

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
+/**
+ * my gulp file
+ * @author chen
+ * @email chen.orange@aliyun.com
+ * @website http://findchen.com
+ */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
@@ -9,6 +16,7 @@ var gulpIf = require('gulp-if');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
 
+/** sass **/
 gulp.task('sass', function(){
     return gulp.src('app/scss/*.scss')
         .pipe(sass({
@@ -17,33 +25,37 @@ gulp.task('sass', function(){
               'bower_components/motion-ui/src',
               'bower_components/font-awesome/scss'
             ]
-        }))
+        }).on('error', sass.logError))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream:true}));
 });
 
+/** js **/
 gulp.task('js', function(){
     return gulp.src('app/js/*.js')
         .pipe(browserSync.reload({stream:true}));
 });
 
+/** images **/
 gulp.task('images', function(){
-    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+    return gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'));
+        .pipe(gulp.dest('dist/img'));
 });
 
+/** fonts **/
 gulp.task('fonts', function(){
     return gulp.src('app/fonts/**/*')
         .pipe(gulp.dest('dist/fonts'));
 });
 
+/** clean for build **/
 gulp.task('clean', function(){
     return del.sync('dist');
 });
 
 
-/** build task **/
+/** useref **/
 gulp.task('useref', function(){
     return gulp.src('app/*.html')
         .pipe(useref())
@@ -52,8 +64,9 @@ gulp.task('useref', function(){
         .pipe(gulp.dest('dist'));
 });
 
+/** nunjucksRender **/
 gulp.task('nunjucksRender', function(){
-    return gulp.src(['app/html/*.html', 'app/template/*.html'])
+    return gulp.src(['app/html/*.html'])
         .pipe(nunjucksRender({
             path: ['app/template/']
         }))
@@ -61,6 +74,7 @@ gulp.task('nunjucksRender', function(){
         .pipe(browserSync.reload({stream:true}));
 });
 
+/** browserSync **/
 gulp.task('browserSync', function(){
     browserSync.init({
         server: {
@@ -69,6 +83,7 @@ gulp.task('browserSync', function(){
     });
 });
 
+/** default **/
 gulp.task('default', ['sass', 'js', 'nunjucksRender', 'browserSync'], function(){
     console.log('Hello Chen');
     gulp.watch('app/scss/*.scss', ['sass']);
@@ -76,6 +91,7 @@ gulp.task('default', ['sass', 'js', 'nunjucksRender', 'browserSync'], function()
     gulp.watch('app/**/*.html', ['nunjucksRender']);
 });
 
-gulp.task('build', ['clean', 'sass', 'nunjucksRender', 'useref', 'images', 'fonts'], function(){
+/** build **/
+gulp.task('build', ['clean', 'sass', 'nunjucksRender', 'fonts', 'images', 'useref'], function(){
     console.log('Building files...');
 });
